b082544dc2b871e1bedc66445dfe21f218b3900a
matiaspizarro024 <pizarro.matias.epet20@gmail.com>
2022-11-13 14:45:40 -0300

    Agrego fuente y tamaño a la letra

--- Js/contact.js
+++ Js/contact.js
@@ -3,7 +3,7 @@ var formfin = document.getElementById("formfin");
 function addElemento(nombre){
   var formfin = document.getElementById("formfin");
   var h1 = document.createElement("h1");
-  var cont = document.createTextNode("¡Su consulta fue recibida!")
+  var cont = document.createTextNode(" ¡Su consulta fue recibida! ")
   h1.innerHTML = nombre;
   formfin.appendChild(h1);
   formfin.appendChild(cont);
