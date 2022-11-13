c25c53f652399383166505e8b7a152ee3097172f
matiaspizarro024 <pizarro.matias.epet20@gmail.com>
2022-11-13 14:29:53 -0300

    Dando estilo a div del onclick

--- Js/contact.js
+++ Js/contact.js
@@ -3,6 +3,7 @@ var formfin = document.getElementById("formfin");
 function addElemento(nombre){
   var formfin = document.getElementById("formfin");
   var h1 = document.createElement("h1");
+  var cont = document.createTextNode("¡Su consulta fue recibida!")
   h1.innerHTML = nombre;
   formfin.appendChild(h1);
 }
