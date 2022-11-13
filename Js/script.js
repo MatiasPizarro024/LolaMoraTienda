f8e2a340cc80b9f2c346153a8552045919bc9c9f
Milivalderrama <milagrosvalderramaepet20@gmail.com>
2022-11-13 11:02:34 -0300

    si el usuario no completa todos los datos el formu nose envia

--- Js/script.js
+++ Js/script.js
@@ -3,3 +3,7 @@ const expresiones = {
     apellido: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
     correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 }
+
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
