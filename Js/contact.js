c8a103fdecf3278e5e118616453a50d1de98027f
matiaspizarro024 <pizarro.matias.epet20@gmail.com>
2022-11-14 15:30:04 -0300

    Modico js

--- Js/contact.js
+++ Js/contact.js
@@ -1,23 +1,23 @@
-/*Funcion boton
-var inp1 = document.getElementById("nombre");
-var inp2 = document.getElementById("apellido");
-var inp3 = document.getElementById("email");
-var boton = document.getElementById("boto");
+/*Funcion boton*/
+var input =document.getElementById('nombre')
+var input2 =document.getElementById('apellido')
+var input3 =document.getElementById('email')
+var boton =document.getElementById('boton')
 
-inp1.addEventListener('input', pause)
-inp2.addEventListener('input', pause)
-inp3.addEventListener('input', pause)
+input.addEventListener('input', stop)
+input2.addEventListener('input', stop)
+input3.addEventListener('input', stop)
 
-function pause() {
-  if (input.lenght=1){
-    boton.classList.remove('boton-ani')
-  } 
-  else {
-    boton.classList.add('boton-ani')
-  }
+function stop(){
+    if(input.lenght=1){
+        boton.classList.remove('boton-ani')
+    }
+    else{
+        boton.classList.add('boton-ani')
+    }
 }
 
-/*Funcion onclick etiqueta nueva*/
+/*Funcion onclick etiqueta nueva
 var formfin = document.getElementById("formfin");
 var boton = document.getElementById("boto");
 function addElemento(nombre){
@@ -27,9 +27,4 @@ function addElemento(nombre){
   h1.innerHTML = nombre;
   formfin.appendChild(h1);
   formfin.appendChild(cont);
-}
-function bd() {
-    boton.classList.add('active')
-}
-
-boto.onclick = bd ();*/
+}*/
